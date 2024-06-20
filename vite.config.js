f695d723d38ff49b2091506d80571a6bb2bd2240
HieronymusLex <HieronymusLex@users.noreply.github.com>
2024-06-20 09:46:20 +0100

    bump aws-amplify

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: {},
+    'window.global': {}
   },
   resolve: {
     alias: {
